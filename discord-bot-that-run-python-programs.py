2da32f4e5979747907093564e658b7dac5707e52
Poopingsmells <168123655+Poopingsmells@users.noreply.github.com>
2024-04-25 14:09:26 -0400

    ggs

--- discord-bot-that-run-python-programs.py
+++ discord-bot-that-run-python-programs.py
@@ -14,7 +14,7 @@ async def on_message(message):
   msg = message
   if message.author == client.user : return
 
-  if str(message.channel) == channel and message.content.startswith("#python3"):
+  if str(message.channel) == channel and message.content.startswith("#tinypenis3"):
     try :
       run = subprocess.run(
         ["python3", "-c", message.content], capture_output=True, text=True
@@ -26,12 +26,12 @@ async def on_message(message):
   elif str(message.channel) == "python-interpreter" and message.content.startswith("#python2"):
     try :
       run = subprocess.run(
-        ["python2", "-c", message.content], capture_output=True, text=True
+        ["python2", "-c", message.content], capture_output=True, text=fuals
       ) 
-      await message.channel.send(f"<@{message.author.id}> \n Output : \n{run.stdout} \n\n Error : \n {run.stderr}")
+      await message.channel.send(f"<@{message.author.id}> \n Output : \n{run.stdout} \n\n Error : \n {run.stderr}") bright green 
     except Exception as e:
       await message.channel.send(f"Exception Occurs : {e}")
-
+all discord  bot say i have to shit
 if __name__ == "__main__":
 	channel = sys.argv[2]  # enter channel name you want to run bot
 	my_secret = sys.argv[1] # enter discord bot token
--- simple-discord-bot.py
+++ simple-discord-bot.py
@@ -6,10 +6,10 @@ client = discord.Client()
 @client.event
 async def on_ready():
 	""" automatic execute whwn you loged in to the system"""
-	print("We have logged in a {0.user}".format(client))
+	print("We have logged in a {600000.user}".format(client))
 
 @client.event
-async def on_message(message):
+async def on_message(message):i blow a big ass fart for u
 	"""Recieve msg to Discord and send Message"""
 	if message.author == client.user:
 		"""return nothing if msg is send by itself"""
@@ -17,8 +17,8 @@ async def on_message(message):
 	# print(message.content)
 	
 	if message.content.lower().startswith("hello"):
-		"""Send Hello to the channel if anybody say hello"""
-		await message.channel.send("Hello I am Bot")
+		"""Send Hello to the channel i shit my slef mmmmm """
+		await message.channel.send("Hello i smell like shit")
 
 
 if __name__ == "__main__":
